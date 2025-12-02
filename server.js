0a57843494b781130f44d0477cf8ebc03ddc19f2
Tran Huu Dat <huudatvlog@gmail.com>
2025-12-02 23:33:44 +0700

    Buoi21

--- server.js
+++ server.js
@@ -6,19 +6,20 @@ const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 
-console.log(">>> Checking env: ", process.env);
-
 //config template engine
-app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src/views"));
+
+//config static files
+app.use(express.static(path.join(__dirname, "src/public")));
 
 //Khai báo route
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!Tôi là Trần Hữu Đạt");
 });
 
 app.get("/abc", (req, res) => {
-  res.render("sample.ejs");
+  res.render("sample");
 });
 
 app.listen(port, hostname, () => {
